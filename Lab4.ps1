295f40c59fdb964b2a7c3781a350699adef3d570
Nick Rimmer <nick.rimmer.2009@gmail.com>
2020-05-10 17:21:06 +0100

    Upload of Sample Files

--- Lab4.ps1
+++
@@ -1,4 +0,0 @@
-New-AzResourceGroupDeployment -name "30042020_Deployment" -ResourceGroupName "SC_ARM" -TemplateFile .\functionsExamples_EP4.json
-
-
-New-AzResourceGroupDeployment -name "30042020_Deployment" -ResourceGroupName "SC_ARM" -TemplateFile .\template.json -TemplateParameterFile .\template.parameter.json
